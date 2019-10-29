d7d5f43e52b1eb739f9593024ee93c4d219d1bf6
Terry <caixin0823@gmail.com>
2019-10-29 10:34:48 +0800

    測試套件

--- backend/application/controllers/Test.php
+++ backend/application/controllers/Test.php
@@ -1,5 +1,8 @@
 <?php defined('BASEPATH') || exit('No direct script access allowed');
 
+use Plunar\Plunar;
+use Plunar\PlunarException;
+
 class Test extends CI_Controller
 {
     public function __construct()
@@ -11,5 +14,20 @@ class Test extends CI_Controller
     {
         $this->load->driver('cache');
         $this->cache->redis->clean();
+    }
+    
+    public function calendar()
+    {
+        echo mktime(0, 0, 0, 2, 9, 2100);
+        exit();
+        try {
+            //支持字符串输入形式 Plunar::solarToLunar('1984-09-22');
+            $lunar_array = Plunar::solarToLunar(2020, 1, 25);
+        } catch (PlunarException $e) {
+            echo $e->getMessage();
+            exit;
+        }
+        
+        var_dump($lunar_array);
     }
 }
